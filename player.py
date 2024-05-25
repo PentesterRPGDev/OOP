04d41ec07177e9bba5b9bf9b31807e06881ea825
Ricardo Cravo Development <153399311+PentesterRPGDev@users.noreply.github.com>
2024-05-25 09:34:11 +0000

    feat: fighting

--- player.py
+++ player.py
@@ -1,21 +1,43 @@
-import time
+"""
+Loads everything beloging to the player
+"""
+
 import random
+import time
 
 class Player:
+    """ creates a player class """
     def __init__(self, name):
         self.name = name
-        self.atk = 1
-        self.dmg = 1
-        self.hp = 300
-        self.shield = 1
-        self.xp = 2
-    
+        self.atk_lvl = 1
+        self.str_lvl = 1
+        self.lp_lvl = 300
+        self.def_lvl = 1
+        self.xp = 0
+
+    def __str__(self):
+        return f"\n{self.name} has {self.lp_lvl} live points."
+
     @classmethod
     def damage(cls, player):
-        hit = player.atk * (random.randint(1, 10)) * player.dmg
+        """ calculates player's damage """
+        hit = player.atk_lvl * (random.randint(1, 100)) * player.str_lvl
+        slow_print(print(f"\n{player.name} deals {hit} damage."))
         return hit
 
-player1 = Player("Test")
+    @classmethod
+    def leveling(cls, player):
+        pass
 
-hitting = Player.damage(player1)
-print(f"deals {hitting}")
+adventurer = Player(input("\nWhat's your name, dear adventurer? \n\n"))
+print(adventurer)
+hitting = Player.damage(adventurer)
+
+def slow_print(text, delay=0.05):
+    """ prints text slow, simulate reading """
+    for char in text:
+        print(char, end='', flush=True)
+        time.sleep(delay)
+
+# Display the question slowly
+slow_print("What's your name, dear adventurer?", delay=0.2)
